05180b8d553da818d271da9ecd2f3b4e821d3b3a
showy1 <showy141@gmail.com>
2023-10-27 01:04:53 +0900

    add jobResponsibilities and technologies of project to .md

--- scripts/generateMd.ts
+++ scripts/generateMd.ts
@@ -7,9 +7,21 @@ const tableHeaderMd = `| key | value |
 | --- | --- |
 `;
 
+type Technologies = (typeof projects)[number]["technologies"];
+
+const technologyPropertyAndNameMap = new Map<keyof Technologies, string>([
+  ["languages", "言語"],
+  ["iaas", "IaaS"],
+  ["db", "DB"],
+  ["mainFrameworks", "フレームワーク・主要ライブラリ"],
+  ["iaasServices", "IaaS 内サービス"],
+  ["others", "その他"],
+]);
+
 class MdGenerator {
   private template: string;
   private projectTemplate: string;
+  private technologiesTemplate: string;
 
   constructor() {
     this.template = readFileSync(`${__dirname}/template.md`, "utf-8");
@@ -17,6 +29,20 @@ class MdGenerator {
       `${__dirname}/projectTemplate.md`,
       "utf-8"
     );
+
+    let t = "";
+    for (const [key, value] of technologyPropertyAndNameMap) {
+      t += Mustache.render(
+        "##### {{ value }}\n\n{{ open }} {{ key }} {{ close }}\n\n",
+        {
+          value,
+          key,
+          open: "{{",
+          close: "}}",
+        }
+      );
+    }
+    this.technologiesTemplate = t;
   }
 
   run() {
@@ -38,7 +64,22 @@ class MdGenerator {
   private convProjectsToMd() {
     let output = "";
     for (const e of projects) {
-      output += Mustache.render(this.projectTemplate, e);
+      output += Mustache.render(this.projectTemplate, {
+        ...e,
+        jobResponsibilities: this.arrToMd(e.jobResponsibilities),
+        technologies: Mustache.render(
+          this.technologiesTemplate,
+          e.technologies
+        ),
+      });
+    }
+    return output;
+  }
+
+  private arrToMd(arr: string[]) {
+    let output = "";
+    for (const e of arr) {
+      output += `- ${e}\n`;
     }
     return output;
   }
--- scripts/projectTemplate.md
+++ scripts/projectTemplate.md
@@ -9,4 +9,12 @@
 
 {{ serviceOverview }}
 
+#### 担当業務
+
+{{ jobResponsibilities }}
+
+#### 使用技術
+
+{{ technologies }}
+
 </details>
